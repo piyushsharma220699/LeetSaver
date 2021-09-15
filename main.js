6c96d598bda292f5776e91c77dfa556184e4fbea
Piyush Sharma <piyushsharma220699@gmail.com>
2021-09-15 17:15:25 +0530

    Anchor Tags working

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+var SOURCE_CODE = "https://github.com/piyushsharma220699/LeetSaver";
+
+function open_github(){
+    window.open(SOURCE_CODE,"_blank");
+}
+
+document.getElementById("linktocode").addEventListener("click",open_github);
+
+// GET THE URL OF THE LINK WHERE THE EXTENSION GETS OPENED
+
+var URL;
+
 chrome.tabs.query({active:true},function(tab){
-    url = tab[0].url;
-    console.log(url);
+    URL = tab[0].url;
+    console.log(URL);
 });
